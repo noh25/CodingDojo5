74f4002895641548a57e33070d9f07a456ef8e81
nikolas08 <nikolas.hava@outlook.com>
2018-01-02 22:23:58 +0100

    started xaml frames

---
+++ CodingDojo5_Hava/CodingDojo5_Hava/ViewModel/ItemVM.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CodingDojo5_Hava.ViewModel
+{
+    public class ItemVM : ViewModelBase
+    {
+        public string Description { get; set; }
+
+        public ImageSource ItemImage { get; set; }
+
+        public string AgeRecommendation { get; set; }
+
+        public ItemVM(string description, ImageSource itemImage, string ageRecommendation)
+        {
+            Description = description;
+            ItemImage = itemImage;
+            AgeRecommendation = ageRecommendation;
+        }
+    }
+}
--- CodingDojo5_Hava/CodingDojo5_Hava/ViewModel/WishlistVM.cs
+++ CodingDojo5_Hava/CodingDojo5_Hava/ViewModel/WishlistVM.cs
@@ -1,6 +1,10 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +13,14 @@ namespace CodingDojo5_Hava.ViewModel
 {
     public class WishlistVM : ViewModelBase
     {
+        public ObservableCollection<ItemVM> Items { get; set; }
 
+        Messenger messenger = SimpleIoc.Default.GetInstance<Messenger>();
+
+        public WishListVM()
+        {
+            Items = new ObservableCollection<ItemVM>();
+            //messenger.Register<PropertyChangedMessage<ItemVM>>(this, updateList);
+        }
     }
 }
